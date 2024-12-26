e1edcc657de0fe1964f5976befee3e00110ceda2
Markus Moeller <markus_moeller@compuserve.com>
2024-12-26 17:37:35 +0000

    remove duplication

--- logging/windows.go
+++ logging/windows.go
@@ -3,13 +3,11 @@
 package logging
 
 import (
-	"os"
 	"fmt"
 	"golang.org/x/sys/windows/svc/eventlog"
 	"myproxy/readconfig"
 	"regexp"
 	"strings"
-	"time"
 )
 
 var alreadyExists bool = false
@@ -74,76 +72,8 @@ func Printf(level string, format string, a ...any) (int, error) {
 			length = len("UNKNOWN: " + message)
 		}
 		wlog.Close()
-	} else if strings.ToUpper(readconfig.Config.Logging.File) != "STDOUT" {
-		 var logFile *os.File
-		// Log to local Unix file
-		logFile, err = os.OpenFile(readconfig.Config.Logging.File, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
-                if err != nil {
-                       return 0, err
-                }
-		timeStamp := time.Now().Format(time.RFC1123)
- 		if level == "INFO" {
-			switch {
-			case
-				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
-				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
-				length, err = fmt.Fprintf(logFile,"%s INFO: %s", timeStamp, message)
-			default:
-			}
-		} else if level == "DEBUG" {
-			switch {
-			case
-				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
-				length, err = fmt.Fprintf(logFile,"%s DEBUG: %s", timeStamp, message)
-			default:
-			}
-		} else if level == "WARNING" {
-			switch {
-			case
-				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
-				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
-				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
-				length, err = fmt.Fprintf(logFile,"%s WARNING: %s", timeStamp, message)
-			default:
-			}
-		} else if level == "ERROR" {
-			length, err = fmt.Fprintf(logFile,"%s ERROR: %s", timeStamp, message)
-		} else {
-			length, err = fmt.Fprintf(logFile,"%s UNKNOWN: %s", timeStamp, message)
-		}
-		logFile.Close()
 	} else {
-		// Log to stdout
-		timeStamp := time.Now().Format(time.RFC1123)
-		if level == "INFO" {
-			switch {
-			case
-				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
-				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
-				length, err = fmt.Printf("%s INFO: %s", timeStamp, message)
-			default:
-			}
-		} else if level == "DEBUG" {
-			switch {
-			case
-				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
-				length, err = fmt.Printf("%s DEBUG: %s", timeStamp, message)
-			default:
-			}
-		} else if level == "WARNING" {
-			switch {
-			case
-				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
-				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
-				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
-				length, err = fmt.Printf("%s WARNING: %s", timeStamp, message)
-			default:
-			}
-		} else if level == "ERROR" {
-			length, err = fmt.Printf("%s ERROR: %s", timeStamp, message)
-		} else {
-			length, err = fmt.Printf("%s UNKNOWN: %s", timeStamp, message)
-		}
+                length, err = osPrintf(level, format, a...)
 	}
 	return length, err
 }
