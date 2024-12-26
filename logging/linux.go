4fce6c4bbd70c441bd0658d84dfc1e390b6a875d
Markus Moeller <markus_moeller@compuserve.com>
2024-12-26 15:40:05 +0000

    Add timestamps

--- logging/linux.go
+++ logging/linux.go
@@ -3,52 +3,134 @@
 package logging
 
 import (
+	"os"
 	"fmt"
 	"log/syslog"
 	"myproxy/readconfig"
 	"strings"
+	"time"
 )
 
 func Printf(level string, format string, a ...any) (int, error) {
-	// Log to local Unix syslog socket 
-	// Need to add option to change to udp,tcp, ...
+	var length int = 0
+	var err error = nil
 
-	sysLog, err := syslog.Dial("", "/dev/log",
-		syslog.LOG_WARNING|syslog.LOG_DAEMON, "myproxy")
-	if err != nil {
-		return 0,err
-	} 
 	message := fmt.Sprintf(format, a...)
-	if level == "INFO" {
-		switch {
-			case 
+	if strings.ToUpper(readconfig.Config.Logging.File) == "SYSLOG" || strings.ToUpper(readconfig.Config.Logging.File) == "EVENTLOG" {
+		var sysLog *syslog.Writer
+		// Log to local Unix syslog socket
+		// Need to add option to change to udp,tcp, ...
+
+		sysLog, err = syslog.Dial("", "/dev/log",
+			syslog.LOG_WARNING|syslog.LOG_DAEMON, "myproxy")
+		if err != nil {
+			return 0, err
+		}
+		if level == "INFO" {
+			switch {
+			case
 				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
 				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
 				err = sysLog.Info("INFO: " + message)
+				length = len("INFO: " + message)
 			default:
-		}
-	} else if level == "DEBUG" {
-		switch {
-			case 
+			}
+		} else if level == "DEBUG" {
+			switch {
+			case
 				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
 				err = sysLog.Debug("DEBUG: " + message)
+				length = len("DEBUG: " + message)
 			default:
-		}
-	} else if level == "WARNING" {
-		switch {
-			case 
+			}
+		} else if level == "WARNING" {
+			switch {
+			case
 				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
 				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
 				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
 				err = sysLog.Warning("WARNING: " + message)
+				length = len("WARNING: " + message)
 			default:
+			}
+		} else if level == "ERROR" {
+			err = sysLog.Err("ERROR: " + message)
+			length = len("ERROR: " + message)
+		} else {
+			err = sysLog.Info("UNKNOWN:" + message)
+			length = len("UNKNOWN: " + message)
 		}
-	} else if level == "ERROR" {
-		err = sysLog.Err("ERROR: " + message)
+		sysLog.Close()
+	} else if strings.ToUpper(readconfig.Config.Logging.File) != "STDOUT" {
+		 var logFile *os.File
+		// Log to local Unix file
+		logFile, err = os.OpenFile(readconfig.Config.Logging.File, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+                if err != nil {
+                       return 0, err
+                }
+		timeStamp := time.Now().Format(time.RFC1123) 
+		if level == "INFO" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
+				length, err = fmt.Fprintf(logFile,"%s INFO: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "DEBUG" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
+				length, err = fmt.Fprintf(logFile,"%s DEBUG: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "WARNING" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
+				length, err = fmt.Fprintf(logFile,"%s WARNING: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "ERROR" {
+			length, err = fmt.Fprintf(logFile,"%s ERROR: %s", timeStamp, message)
+		} else {
+			length, err = fmt.Fprintf(logFile,"%s UNKNOWN: %s", timeStamp, message)
+		}
+		logFile.Close()
 	} else {
-		err = sysLog.Info("UNKNOWN:" + message)
+		// Log to stdout
+		timeStamp := time.Now().Format(time.RFC1123) 
+		if level == "INFO" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
+				length, err = fmt.Printf("%s INFO: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "DEBUG" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
+				length, err = fmt.Printf("%s DEBUG: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "WARNING" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
+				length, err = fmt.Printf("%s WARNING: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "ERROR" {
+			length, err = fmt.Printf("%s ERROR: %s", timeStamp, message)
+		} else {
+			length, err = fmt.Printf("%s UNKNOWN: %s", timeStamp, message)
+		}
 	}
-	sysLog.Close()
-	return len(message),err
+	return length, err
 }
-
--- logging/windows.go
+++ logging/windows.go
@@ -3,71 +3,147 @@
 package logging
 
 import (
+	"os"
 	"fmt"
 	"golang.org/x/sys/windows/svc/eventlog"
 	"myproxy/readconfig"
 	"regexp"
 	"strings"
+	"time"
 )
 
 var alreadyExists bool = false
 
 func Printf(level string, format string, a ...any) (int, error) {
-	var err error
+	var length int = 0
+	var err error = nil
 	var loggerName string = "myproxy"
 	var wlog *eventlog.Log
 
-	// Log to local windows eventlog
-	if !alreadyExists {
-		err = eventlog.InstallAsEventCreate(loggerName, eventlog.Info|eventlog.Warning|eventlog.Error)
-		if err != nil {
-			alreadyExists, _ = regexp.MatchString(" registry key already exists", err.Error())
-			if !alreadyExists {
-				return 0, err
-			}
-		}
-		alreadyExists = true
-	}
-
-	wlog, err = eventlog.Open(loggerName)
-	if err != nil {
-		return 0, err
-	}
-
 	message := fmt.Sprintf(format, a...)
-	if level == "INFO" {
-               switch {
-                        case
-                                strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
-                                strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
+	if strings.ToUpper(readconfig.Config.Logging.File) == "SYSLOG" || strings.ToUpper(readconfig.Config.Logging.File)  == "EVENTLOG" {
+		// Log to local windows eventlog
+		if !alreadyExists {
+			err = eventlog.InstallAsEventCreate(loggerName, eventlog.Info|eventlog.Warning|eventlog.Error)
+			if err != nil {
+				alreadyExists, _ = regexp.MatchString(" registry key already exists", err.Error())
+				if !alreadyExists {
+					return 0, err
+				}
+			}
+			alreadyExists = true
+		}
+
+		wlog, err = eventlog.Open(loggerName)
+		if err != nil {
+			return 0, err
+		}
+
+		if level == "INFO" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
 				err = wlog.Info(100, "INFO: "+message)
-                        default:
-                }
-	} else if level == "DEBUG" {
-               switch {
-                        case
-                                strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
+				length = len("INFO: " + message)
+			default:
+			}
+		} else if level == "DEBUG" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
   				err = wlog.Info(700, "DEBUG: "+message)
-                        default:
-                }
-	} else if level == "WARNING" {
-               switch {
-                        case
-                                strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
-                                strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
-                                strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
+				length = len("DEBUG: " + message)
+			default:
+			}
+		} else if level == "WARNING" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
 				err = wlog.Warning(200, "WARNING: "+message)
-                        default:
+				length = len("WARNING: " + message)
+			default:
+			}
+		} else if level == "ERROR" {
+			err = wlog.Error(300, "ERROR: "+message)
+			length = len("ERROR: " + message)
+		} else {
+			err = wlog.Info(500, "UNKNONW: "+message)
+			length = len("UNKNOWN: " + message)
+		}
+		wlog.Close()
+	} else if strings.ToUpper(readconfig.Config.Logging.File) != "STDOUT" {
+		 var logFile *os.File
+		// Log to local Unix file
+		logFile, err = os.OpenFile(readconfig.Config.Logging.File, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+                if err != nil {
+                       return 0, err
                 }
-	} else if level == "ERROR" {
-		err = wlog.Error(300, "ERROR: "+message)
+		timeStamp := time.Now().Format(time.RFC1123)
+ 		if level == "INFO" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
+				length, err = fmt.Fprintf(logFile,"%s INFO: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "DEBUG" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
+				length, err = fmt.Fprintf(logFile,"%s DEBUG: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "WARNING" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
+				length, err = fmt.Fprintf(logFile,"%s WARNING: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "ERROR" {
+			length, err = fmt.Fprintf(logFile,"%s ERROR: %s", timeStamp, message)
+		} else {
+			length, err = fmt.Fprintf(logFile,"%s UNKNOWN: %s", timeStamp, message)
+		}
+		logFile.Close()
 	} else {
-		err = wlog.Info(500, "UNKNONW: "+message)
+		// Log to stdout
+		timeStamp := time.Now().Format(time.RFC1123)
+		if level == "INFO" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO":
+				length, err = fmt.Printf("%s INFO: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "DEBUG" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG":
+				length, err = fmt.Printf("%s DEBUG: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "WARNING" {
+			switch {
+			case
+				strings.ToUpper(readconfig.Config.Logging.Level) == "DEBUG",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "INFO",
+				strings.ToUpper(readconfig.Config.Logging.Level) == "WARNING":
+				length, err = fmt.Printf("%s WARNING: %s", timeStamp, message)
+			default:
+			}
+		} else if level == "ERROR" {
+			length, err = fmt.Printf("%s ERROR: %s", timeStamp, message)
+		} else {
+			length, err = fmt.Printf("%s UNKNOWN: %s", timeStamp, message)
+		}
 	}
-	wlog.Close()
-	if err != nil {
-		return 0, err
-	} else {
-		return len(message), err
-	}
+	return length, err
 }
