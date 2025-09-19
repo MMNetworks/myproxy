6f4b023fcbd632acf23b9e32272ccf21612492fc
Markus Moeller <markus.moeller@breakingwave.io>
2025-09-19 15:34:14 +0100

    Move Version number to config

--- version.go
+++ version.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"time"
+	"myproxy/service"
 )
-
-const Version = "2.5.0"
 
 func init() {
 	timeStamp := time.Now().Format(time.RFC1123)
-	fmt.Printf("%s INFO: main: Starting version: %s\n", timeStamp, Version)
+	fmt.Printf("%s INFO: main: Starting version: %s\n", timeStamp, service.Version)
 
 }
